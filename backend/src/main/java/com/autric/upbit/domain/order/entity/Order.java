0bfaa6228bfbbe9ec9897a8d648bc38fffa6dca7
HanSeongHyun <tjdgus4560@naver.com>
2025-06-09 18:05:59 +0900

    feat(entity) : Orders Entity 추가

--- backend/src/main/java/com/autric/upbit/domain/order/entity/Order.java
+++
@@ -1,7 +0,0 @@
-package com.autric.upbit.domain.order.entity;
-
-/**
- * 주문 내역
- */
-public class Order {
-}
---
+++ backend/src/main/java/com/autric/upbit/domain/order/entity/Orders.java
@@ -0,0 +1,114 @@
+package com.autric.upbit.domain.order.entity;
+
+import com.autric.upbit.domain.chart.entity.Market;
+import com.autric.upbit.domain.member.entity.Member;
+import com.autric.upbit.domain.signal.entity.MemberSignal;
+import com.autric.upbit.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+/**
+ * 주문 내역
+ */
+@Entity
+@Table(name = "orders")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Orders extends BaseTimeEntity {
+
+    /**
+     * 주문ID PK
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "orders_id")
+    private Long id;
+
+    /**
+     * 멤버ID FK
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    /**
+     * 마켓ID FK
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "market_id")
+    private Market market;
+
+    /**
+     * 멤버시그널ID FK
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_signal_id")
+    private MemberSignal memberSignal;
+
+    /**
+     * 주문 UUID (주문 성공 시)
+     */
+    @Column(length = 64, unique = true)
+    private String uuid;
+
+    /**
+     * 주문 방향 (Enum)
+     */
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private Side side;
+
+    /**
+     * 주문유형 (Enum)
+     */
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ord_type", length = 10)
+    private OrderType ordType;
+
+    /**
+     * 주문 수량
+     */
+    @Column(precision = 24, scale = 12)
+    private BigDecimal volume;
+
+    /**
+     * 주문가 (시장가 주문일 경우 NULL 가능)
+     */
+    @Column(precision = 18, scale = 8)
+    private BigDecimal price;
+
+    /**
+     * 주문 요청 처리 결과 상태 (Enum)
+     */
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private OrderStatus status;
+
+    /**
+     * 주문 실패 시 오류 메시지 내용
+     */
+    @Column(name = "error_message")
+    private String errorMessage;
+
+
+    @Builder
+    public Orders(Member member, Market market, MemberSignal memberSignal, String uuid, Side side,
+                  OrderType ordType, BigDecimal volume, BigDecimal price,
+                  OrderStatus status, String errorMessage) {
+
+        this.member = member;
+        this.market = market;
+        this.memberSignal = memberSignal;
+        this.uuid = uuid;
+        this.side = side;
+        this.ordType = ordType;
+        this.volume = volume;
+        this.price = price;
+        this.status = status;
+        this.errorMessage = errorMessage;
+    }
+
+}
